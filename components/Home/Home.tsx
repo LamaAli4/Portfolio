b645ae43028b4dac3b67d5e60e8e8a915821c62c
Lama Emad Ali <lama2015678@gmail.com>
2025-08-02 12:58:28 +0300

    Make footer section and make the download button

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -5,7 +5,6 @@ import Resume from "./Resume/Resume";
 import Projects from "./Projects/Projects";
 import Skills from "./Skills/Skills";
 import Contact from "./Contact/Contact";
-import Footer from "./Footer/Footer";
 
 const Home = () => {
   return (
